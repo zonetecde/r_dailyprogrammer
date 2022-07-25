2caffdb2fda8a753ecd0aedd2f18225e35f69348
zonedetec@gmail.com <zonedetec@gmail.com>
2022-07-25 15:26:25 +0200

    Ajoutez des fichiers projet.

---
+++ Challenge 399 - Letter value sum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_399___Letter_value_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
