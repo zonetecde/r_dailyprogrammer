586a673ceeb5a55682e2d10ee7ed5b23e4ddbf3e
zonedetec@gmail.com <zonedetec@gmail.com>
2022-07-30 18:09:41 +0200

    Challenge 386

--- Challenge 386 - Partition counts/Program.cs
+++ Challenge 386 - Partition counts/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Text;
@@ -22,56 +23,43 @@ namespace Challenge_386___Partition_counts
             // --------------- CALCUL DE LA SUITE LOGIQUE POUR ADDITION/SOUSTRACTION
             List<int> posOfCalcul_temp = new List<int>() { 1, 3 };
 
-            for (int i = 3; i <= v; i++)
-            {
-                if(i%2 == 1)
-                {
-                    posOfCalcul_temp.Add(posOfCalcul_temp[posOfCalcul_temp .Count - 2] + 1);
-                }
-                else
-                {
+            for (int i = 3; i <= v; i++)         
+                if(i%2 == 1)               
+                    posOfCalcul_temp.Add(posOfCalcul_temp[posOfCalcul_temp .Count - 2] + 1);               
+                else               
                     posOfCalcul_temp.Add(posOfCalcul_temp[posOfCalcul_temp.Count - 2] + 2);
-                }
-            }
+                          
+            List<CalculatorPos> posOfCalcul = new List<CalculatorPos>() { 
+                new CalculatorPos(){pos = 1, isItPlus = true},
+                new CalculatorPos(){pos = 2, isItPlus = true} 
+            };
 
-            List<CalculatorPos> posOfCalcul = new List<CalculatorPos>() { new CalculatorPos()
-            {
-                pos = 1,
-                isItPlus = true
-            },new CalculatorPos()
-            {
-                pos = 2,
-                isItPlus = true
-            } };
-            for (int i = 2; i < posOfCalcul_temp.Count; i++)
-            {
+            for (int i = 2; i < posOfCalcul_temp.Count; i++)         
                 posOfCalcul.Add(new CalculatorPos()
                 {
                     pos = posOfCalcul[i - 1].pos + posOfCalcul_temp[i - 1],
                     isItPlus = posOfCalcul[i - 1].isItPlus == posOfCalcul[i - 2].isItPlus ? !posOfCalcul[i - 1].isItPlus : posOfCalcul[i - 1].isItPlus
                 });
-            }
-
+            
             // ----------------- PARTITION NUMBER SEQUENCE CALCULATOR
             List<BigInteger> logicalSequence = new List<BigInteger>() { 1, 1 };
 
             for (int i = 2; i < v + 1; i++)
             {
                 BigInteger nextInteger = 0;
+
                 for (int a = 0; a < logicalSequence.Count; a++)
                 {
-                    if (posOfCalcul.Exists(x => x.pos - 1 == a))
-                    {
-                        if (posOfCalcul.Find(x => x.pos - 1 == a).isItPlus)
-                            nextInteger += logicalSequence[a];
-                        else
-                            nextInteger -= logicalSequence[a];
+                    var z = posOfCalcul.Find(x => x.pos - 1 == a) ?? new CalculatorPos() { pos = -1, isItPlus = true }; 
 
-                    }
+                    if (z.isItPlus && z.pos != -1)
+                        nextInteger += logicalSequence[a];
+                    else if(z.pos != -1)
+                        nextInteger -= logicalSequence[a];
                 }
+                                  
                 logicalSequence.Insert(0, nextInteger);
             }
-
 
             return logicalSequence.First();
         }
